a69917c56bc8ca29fd150adfdb13cc8d5de33384
ygc <937601471@qq.com>
2021-04-18 20:29:19 +0800

    CROS方案

---
+++ frank-com/public/frank.js
@@ -0,0 +1,20 @@
+function ajax(method, url) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open(method, url);
+    request.onreadystatechange = () => {
+      if (request.readyState === 4) {
+        if (request.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request);
+        }
+      }
+    };
+    request.send();
+  });
+}
+ajax("get", "http://qq.com:8888/friends.json").then(response => {
+  console.log("这是 AJAX");
+  console.log(response);
+});
